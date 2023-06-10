2cd35d4054e5133d61dc82f0c0494fa918708584
Parmeshwar Mokashe <55226812+prem2004@users.noreply.github.com>
2023-06-11 03:20:24 +0530

    new logic added

--- demo.js
+++ demo.js
@@ -32,3 +32,53 @@ console.timeEnd()
 console.time();
 console.log(ss(10,20))
 console.timeEnd()
+
+
+//Q2 impliment this code
+//const result=calc.add(10).miltiply(5).substract(30).add(10)
+//console.log(result.totle)
+
+const calc={
+    result:0,
+    add:function(num){
+        this.result+=num
+        return this
+    },
+    multiply:function(num){
+        this.result=this.result * num
+        return this
+    },
+    substract:function(num){
+        this.result=this.result - num
+        return this
+    }
+}
+
+calc.add(10).multiply(10).substract(30).add(10)
+console.log(calc.result)
+
+//Q 3 flaten below array
+
+function customFlat(arr,dept=1){
+    let result=[]
+    arr.forEach(el => {
+        if(Array.isArray(el) && dept>0)
+        {
+            result.push(...customFlat(el,dept-1))
+        }else{
+            result.push(el)
+        }
+    });
+
+    return result
+}
+
+
+let arr=[
+    [1,2],
+    [3,4,[5,6,[7,8],200]],
+    [9,10]
+]
+
+console.log(customFlat(arr,3))
+//output : [1, 2, 3, 4, 5, 6, 7, 8, 200, 9, 10]
